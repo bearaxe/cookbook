a91a4284975392aef00518495368fbef15b68eb8
shanleigh.mcmanus <shanleigh.mcmanus@helloworld.com>
2017-07-28 13:40:40 -0400

    remove unused code from shopping-edit.component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
-import { Ingredient } from '../../shared/Ingredient.model';
+import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription';
@@ -10,8 +10,6 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
-  // @ViewChild('nameInput') nameInput: ElementRef;
-  // @ViewChild('amountInput') amountInput: ElementRef;
   @ViewChild('f') ingredientForm: NgForm;
 
   subscription: Subscription;
@@ -38,7 +36,6 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   addItem(){
-    // console.log('Add button caught');
     const newIngredient = new Ingredient(this.ingredientForm.value.nameInput,
                                           this.ingredientForm.value.amountInput);
     if(this.editMode){
@@ -48,51 +45,14 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     }
     this.clearItem();
 
-
-    // Old version doing what I thought. Updated to match course.
-    // console.log('uhgm', this.ingredientForm)
-    // const ingName = this.ingredientForm.value.nameInput;
-    // const ingAmount = this.ingredientForm.value.amountInput;
-    // const obj = { name: ingName,
-    //               amount: ingAmount
-    //             };
-    // if( ingName && ingAmount ) {
-    //   if(this.editMode) {
-    //     this.slService.deleteFromList(obj);
-    //   }
-    //   this.slService.addToList(obj);
-    // }
-    // this.clearItem();
-
-      // Old nonForm centered approach
-      // const ingName = this.nameInput.nativeElement.value
-      // const ingAmount = this.amountInput.nativeElement.value
-      // if( ingName !== '' && ingAmount !== '') {
-      //     this.slService.addToList({
-      //         name: ingName,
-      //         amount: ingAmount
-      //     });
-      // }
   }
 
   deleteItem() {
     this.slService.deleteFromList(this.editedItemIndex);
     this.clearItem();
-    // console.log('Delete button caught');
-    // const ingName = this.ingredientForm.value.nameInput;
-    // const ingAmount = this.ingredientForm.value.amountInput;
-    // // This is intentional, idc if the ingAmount is empty or not
-    // if (ingName ) {
-    //   this.slService.deleteFromList({
-    //     name: ingName,
-    //     amount: ingAmount
-    //   });
-    // }
-    // this.clearItem();
   }
 
   clearItem() {
-    // console.log('Clear button caught');
     this.ingredientForm.reset();
     this.editMode = false;
   }
